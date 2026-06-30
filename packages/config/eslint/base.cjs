0d3d71c913edef4392d098504c8caaa6e6d62dfd
Etsuya Akimoto <etsuya233@outlook.com>
2026-06-30 22:25:19 +0800

    build(workspace): add monorepo tooling

---
+++ packages/config/eslint/base.cjs
@@ -0,0 +1,20 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+);
